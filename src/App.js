a06d5cca4a3b1e32df62813ce26d9c6e5322f230
AR456789 <94092324+AR456789@users.noreply.github.com>
2022-03-27 19:16:22 -0700

    Added the project to GitHub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState } from "react";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("");
+  const [icon, setIcon] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=333d1e7c5f22cf63ff65c69aa26537e4&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+
+  function updateSearch(event) {
+    setSearch(event.target.value);
+  }
+
+  function showTemperature(response) {
+    setMessage(
+      <strong>
+        City: {search}
+        <br />
+        Temperature: {Math.round(response.data.main.temp)}˚C
+        <br />
+        Description: {response.data.weather[0].description}
+        <br />
+        Humidity: {response.data.main.humidity} %
+        <br />
+        Wind: {Math.round(response.data.wind.speed)} mph
+      </strong>
+    );
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather["0"].icon}@2x.png`
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="type a city name"
+          autoFocus={true}
+          onChange={updateSearch}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <p>
+        {" "}
+        {message} <br /> <img src={icon} alt=""></img>{" "}
+      </p>
     </div>
   );
 }
